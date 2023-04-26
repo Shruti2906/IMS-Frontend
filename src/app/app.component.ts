58c993c5cda86b59c4406882e6423e18404edd35
Shruti2906 <65824891+Shruti2906@users.noreply.github.com>
2023-04-26 16:58:55 +0530

    user register login ready to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IMS-Frontend';
+
+  email: string = '';
+  password: string = '';
+  registrationSuccess: boolean = false;
+  loginSuccess: boolean = false;
+
+  registrationFail: String = '';
+  loginFail: String = '';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmitReg() {
+    const data = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post('https://ims-backend-bxe0.onrender.com/apis/users/register', data).subscribe(
+      (response) => {
+        console.log('Registration successful.');
+        this.loginSuccess = false;
+        this.registrationSuccess = true;
+        this.registrationFail = '';
+      },
+      (error) => {
+        console.error('Registration failed:', error);
+        this.loginSuccess = false;
+        this.registrationFail = error;
+      }
+    );
+  }
+
+
+  onSubmitLogin() {
+    const data = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post('https://ims-backend-bxe0.onrender.com/apis/users/login', data).subscribe(
+      (response) => {
+        console.log('Login successfull.!');
+        this.registrationSuccess = false; // reset registration success message
+        this.registrationFail = ''; // reset registration failure message
+        this.loginSuccess = true;
+        this.loginFail = '';
+      },
+      (error) => {
+        console.error('Login failed:', error),
+          (this.registrationSuccess = false); // reset registration success message
+        this.loginFail = error;
+        this.registrationFail = ''; // reset registration failure message
+      }
+    );
+
+
+  }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
