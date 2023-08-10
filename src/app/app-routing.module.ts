3a63541be61471c6a8e9a88b8eb2cf782bbda5de
Vaibhavi Pawar <vaibhavi.pawar@linkcode.in>
2023-08-10 07:56:26 +0530

    Added confirm password input field and changed size of register, login block

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path:"auth", loadChildren:()=>import('./auth/auth.module').then(mod=>mod.AuthModule)},
+  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path:'aboutus', component:AboutusComponent}
 ];
