dbe31b2ac222a325b0c6d2ca84205f9ec13aeb06
Shruti Patil <65824891+Shruti2906@users.noreply.github.com>
2023-08-17 16:25:29 +0530

    minor changes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,7 +4,6 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
-import { StudentDashboardComponent } from '../dashboard/course/student-dashboard/student-dashboard.component';
 
 // const routes: Routes = [
 //   {path: 'register', component: RegisterComponent},
@@ -47,7 +46,6 @@ const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'aboutus', component: AboutusComponent },
-      { path: 'studentdashboard', component: StudentDashboardComponent },
     ],
   },
 ];
