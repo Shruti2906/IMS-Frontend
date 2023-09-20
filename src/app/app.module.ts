de446bcc66320b459cdc2d9aa5b16806fec34df8
Shruti Patil <65824891+Shruti2906@users.noreply.github.com>
2023-09-20 10:19:04 +0530

    loader interceptor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 // import { FooterComponent } from './auth/footer/footer.component';
 // import { NavbarComponent } from './auth/navbar/navbar.component';
 // import { AboutusComponent } from './auth/aboutus/aboutus.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { TokenInterceptor } from './interceptor/token.interceptor';
+import { LoaderInterceptor } from './interceptor/loader.interceptor';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -28,9 +29,17 @@ import { TokenInterceptor } from './interceptor/token.interceptor';
     FormsModule,
     AuthModule,
     DashboardModule,
-    // RouterModule.forRoot([])
+    NgxUiLoaderModule.forRoot({
+      bgsColor: '#2196f3',
+      bgsType: 'ball-spin-clockwise-fade-rotating',
+      pbColor: '#2196f3',
+      fgsType: 'ball-spin-clockwise-fade-rotating',
+      hasProgressBar: false,
+    }),
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+              { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptor/loader.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+  constructor(private ngxLoader: NgxUiLoaderService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.ngxLoader.startLoader('loader-1'); // Show the loader
+
+    return next.handle(req).pipe(
+      finalize(() => {
+        this.ngxLoader.stopLoader('loader-1'); // Hide the loader
+      })
+    );
+  }
+}
+
